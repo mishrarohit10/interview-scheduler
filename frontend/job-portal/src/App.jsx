27767e0bc18671bd1baf16e0921e0bc70ad552af
rohit <mishrarohit316x@gmail.com>
2024-10-21 01:22:18 +0530

    bug fixes

--- frontend/job-portal/src/App.jsx
+++ frontend/job-portal/src/App.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Signup from './components/Auth/Signup';
 import Login from './components/Auth/Login';
 import VerifyEmail from './components/Auth/VerifyEmail';
 import VerifySMS from './components/Auth/VerifySMS';
 import CreateJob from './components/Job/CreateJob';
-import Dashboard from './components/Job/Dashboard'; // Import the Dashboard component
+import Dashboard from './components/Job/Dashboard'; 
 import NavBar from './components/NavBar/NavBar';
 import Verify from './components/Auth/Verify';
 
 const App = () => {
     return (
         <Router>
-            <NavBar/>
+            <NavBar />
             <Routes>
-            <Route path="/" element={<Navigate to="/signup" />} />
+                <Route path="/" element={<Navigate to="/signup" />} /> 
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/verify-email/:token" element={<VerifyEmail />} />
                 <Route path="/verify-sms" element={<VerifySMS />} />
                 <Route path="/verify" element={<Verify />} />
                 <Route path="/jobs/create" element={<CreateJob />} />
-                <Route path="/dashboard" element={<Dashboard />} /> 
+                <Route path="/dashboard" element={<Dashboard />} />
             </Routes>
         </Router>
     );
