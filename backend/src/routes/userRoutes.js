427fe0a5965179d2a1f068c77d76bac832c7cb81
rohit <mishrarohit316x@gmail.com>
2024-10-21 01:01:32 +0530

    bug fix sms

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import { registerUser, loginUser, verifySMS, verifyEmail, getEmail } from '../controllers/userController.js';
+import { registerUser, loginUser, verifyEmail, getEmail } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.post('/signup', registerUser);
 router.post('/login', loginUser);
 router.post('/verify-email', getEmail);
-router.post('/verify-sms', verifySMS);
+// router.post('/verify-sms', verifySMS);
 router.post('/verify-email/:token', verifyEmail);
 
 export default router;
